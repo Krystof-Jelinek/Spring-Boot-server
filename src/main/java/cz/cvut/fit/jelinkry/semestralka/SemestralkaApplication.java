660d85dd957a827d6725f74dff4a4b686fed415e
Krystof Jelinek <krys.jelinek@seznam.cz>
2023-10-24 18:03:58 +0200

    Initial commit

---
+++ src/main/java/cz/cvut/fit/jelinkry/semestralka/SemestralkaApplication.java
@@ -0,0 +1,14 @@
+package cz.cvut.fit.jelinkry.semestralka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemestralkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemestralkaApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/test/java/cz/cvut/fit/jelinkry/semestralka/SemestralkaApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.jelinkry.semestralka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemestralkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
