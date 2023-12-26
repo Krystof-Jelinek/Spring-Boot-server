32443ad59df69fbc794154d01cf30508d4edc8ef
Krystof Jelinek <krys.jelinek@seznam.cz>
2023-12-26 22:51:18 +0100

    forgot to add crossorigin to the new controller

--- src/main/java/cz/cvut/fit/jelinkry/semestralka/controller/DatabaseController.java
+++ src/main/java/cz/cvut/fit/jelinkry/semestralka/controller/DatabaseController.java
@@ -2,12 +2,14 @@ package cz.cvut.fit.jelinkry.semestralka.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import cz.cvut.fit.jelinkry.semestralka.service.DatabaseService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8080")
 public class DatabaseController {
 
     @Autowired
